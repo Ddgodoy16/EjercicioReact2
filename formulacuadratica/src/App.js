9e6d8e5a716327a6a1897861e7e5c3dbc86aabca
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-03-14 21:14:38 -0500

    Codigo Completo

---
+++ formulacuadratica/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {FormulaCuadratica} from './Componentes/FormulaCuadratica';
+
+function App() {
+  return (
+    <div className="App">
+   <FormulaCuadratica></FormulaCuadratica>
+    </div>
+  );
+}
+
+export default App;
